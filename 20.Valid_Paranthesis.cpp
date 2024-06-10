84c18f0bedb082d7bd1eaee6e4f5c106e6711a05
Bilal Shah <93590692+Bilalshah1@users.noreply.github.com>
2024-06-10 20:54:23 +0500

    commit

---
+++ 20.Valid_Paranthesis.cpp
@@ -0,0 +1,55 @@
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> expression;
+
+        vector<pair<char, char>> opposite_pair = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}};
+
+        char opposite;
+        int current;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            {
+                expression.push(s[i]);
+            }
+            if(expression.empty()) return 0;
+            if (s[i] == ')' || s[i] == ']' || s[i] == '}')
+            {
+                for (const auto &pair : opposite_pair)
+                {
+                    if (pair.first == s[i])
+                    {
+                        opposite = pair.second;
+                    }
+                }
+                if (!expression.empty())
+                {
+                    if (expression.top() == opposite)
+                    {
+                        expression.pop();
+                    }
+                    else{
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        if (expression.empty())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
