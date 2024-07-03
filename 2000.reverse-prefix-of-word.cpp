30b9b86fda1da0a9eee2518d4a39d89682f405c9
Bilal Shah <93590692+Bilalshah1@users.noreply.github.com>
2024-07-03 22:46:24 +0500

    Update 2000.reverse-prefix-of-word.cpp
    .

--- 2000.reverse-prefix-of-word.cpp
+++ 2000.reverse-prefix-of-word.cpp
@@ -3,14 +3,9 @@ class Solution
 public:
     string reversePrefix(string word, char ch)
     {
-        for (int i = 0; i < word.size(); i++)
-        {
-            if (ch == word[i])
-            {
-                reverse(word.begin(), word.begin() + i + 1);
-                break;
-            }
-        }
+
+        int i = word.find(ch);
+        reverse(word.begin(), word.begin() + i + 1);
         return word;
     }
 };
