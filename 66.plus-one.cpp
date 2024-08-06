e6277bc6b5da5c1120fdf992400b171f7f1f0175
Bilal Shah <93590692+Bilalshah1@users.noreply.github.com>
2024-08-06 21:10:05 +0500

    Create 66.plus-one.cpp
    .

---
+++ 66.plus-one.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int n = digits.size() - 1;
+        int carry = 0;
+        if (digits[n] != 9)
+            digits[n]++;
+        else
+        {
+            while (n >= 0)
+            {
+                if (digits[n] == 9)
+                {
+                    digits[n] = 0;
+                    carry=1;
+                }
+
+                else
+                {
+                    digits[n]++;
+                    carry=0;
+                    break;
+                }
+                n--;
+            }
+        }
+        if(carry==1) digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
