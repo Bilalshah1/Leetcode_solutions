6815b7987a3c68cd35bf8f1b5fbcaefc49c0fc6f
Bilal Shah <93590692+Bilalshah1@users.noreply.github.com>
2024-08-09 22:22:20 +0500

    Create 118.pascals-triangle.cpp
    .

---
+++ 118.pascals-triangle.cpp
@@ -0,0 +1,31 @@
+ long long int nCr(int n, int r) {
+    if (r > n) return 0;
+    if (r == 0 || r == n) return 1;
+    if (r > n - r) r = n - r;  // Take advantage of symmetry
+     long long int numerator = 1;
+     long long int denominator = 1;
+    for (int i = 0; i < r; ++i) {
+        numerator *= (n - i);
+        denominator *= (i + 1);
+    }
+    return numerator / denominator;
+}
+
+class Solution
+{
+public:
+    vector<vector<int>> generate(int numRows)
+    {
+        vector<vector<int>> Pascal_sequence;
+        for (int i = 0; i < numRows; i++)
+        {
+            Pascal_sequence.push_back(vector<int>());
+            for (int j = 0; j <= i; j++)
+            {
+
+                Pascal_sequence[i].push_back(nCr(i, j));
+            }
+        }
+        return Pascal_sequence;
+    }
+};
