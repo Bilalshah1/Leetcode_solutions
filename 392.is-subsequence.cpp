1844bc7aa78c0af187c3ed7a93f08cb7ac2fe962
Bilal Shah <93590692+Bilalshah1@users.noreply.github.com>
2024-08-26 09:32:34 +0500

    Create 392.is-subsequence.cpp
    .

---
+++ 392.is-subsequence.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    bool isSubsequence(string s, string t)
+    {
+        int cnt = 0;
+        int j = 0;
+        for (int i = 0; i < t.size(); i++)
+        {
+            if (t[i] == s[j])
+            {
+                j++;
+                cnt++;
+            }
+        }
+        if (cnt == s.size())
+            return true;
+        else
+            return false;
+    }
+};
